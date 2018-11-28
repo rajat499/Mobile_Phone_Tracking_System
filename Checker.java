fd74cc346e44b002d63eb78f93c3243429bf99e0
Rajat Jaiswal <32504597+rajat499@users.noreply.github.com>
2018-11-28 13:26:31 +0530

    Update and rename assn2checker.java to Checker.java

--- Checker.java
+++ Checker.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class assn2checker
+public class Checker
 {
 	public static void main ( String args [])
 	{
